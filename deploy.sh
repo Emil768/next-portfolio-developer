5c68499cd1d69a285c88a7afa2e447deb9367a9b
Emil Murahas <murakhas@list.ru>
2024-10-16 19:43:27 +0300

    feat: add new changes deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Загружаем nvm и активируем нужную версию Node.js
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use 20.18.0
+
 # Переходим в директорию с проектом
 cd /var/next-portfolio-developer
 
@@ -7,10 +12,10 @@ cd /var/next-portfolio-developer
 git pull origin main
 
 # Устанавливаем зависимости
-/root/.nvm/versions/node/v20.18.0/bin/yarn install 
+yarn install 
 
 # Собираем проект
-/root/.nvm/versions/node/v20.18.0/bin/yarn build
+yarn build 
 
 # Перезапускаем приложение с помощью PM2
-/root/.nvm/versions/node/v20.18.0/bin/pm2 restart client || /root/.nvm/versions/node/v20.18.0/bin/pm2start yarn --name "client" -- start
+pm2 restart client || pm2 start yarn --name "client" -- start
