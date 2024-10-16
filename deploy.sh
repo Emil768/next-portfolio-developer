040f98790b5081fe139ed965bd11d21835cff42b
Emil Murahas <murakhas@list.ru>
2024-10-16 18:38:47 +0300

    feat: add deploy sh

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Переходим в каталог с вашим приложением
+cd /var/next-portfolio-developer
+
+# Обновляем репозиторий
+git pull origin main
+
+# Устанавливаем зависимости (на случай, если они обновились)
+yarn install
+
+# Собираем приложение
+yarn build
+
+# Перезапускаем приложение с помощью PM2
+pm2 restart client || pm2 start -n "client" "yarn start"
